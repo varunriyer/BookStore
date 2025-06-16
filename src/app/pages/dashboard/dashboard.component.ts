8cd69d36c2ac2de8a0f562f0e821a8dbd4e3e60f
varunriyer <rvaruniyer@gmail.com>
2025-06-16 15:35:16 +0530

    [Varun]: feat: Integrate AppBar component into Dashboard for improved navigation

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { AppbarComponent } from '../../components/appbar/appbar.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [AppbarComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
