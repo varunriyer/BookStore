dbb0d220e27665f1a332b9455b493f3fb5012cd0
varunriyer <rvaruniyer@gmail.com>
2025-06-16 12:24:26 +0530

    [Varun] Add AppbarComponent with HTML, CSS, and unit tests

---
+++ src/app/components/appbar/appbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appbar',
+  imports: [],
+  templateUrl: './appbar.component.html',
+  styleUrl: './appbar.component.css'
+})
+export class AppbarComponent {
+
+}
