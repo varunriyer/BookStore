268ff38b8384e4953fc99f6dc155ebf1aaf1d5b4
varunriyer <rvaruniyer@gmail.com>
2025-06-17 09:07:23 +0530

    [Varun] feat: Add MatMenuModule to AppBar component for enhanced functionality

--- src/app/components/appbar/appbar.component.ts
+++ src/app/components/appbar/appbar.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-appbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './appbar.component.html',
   styleUrl: './appbar.component.css',
 })
