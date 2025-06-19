1a39dcc129a3d87218dc489fe640c61594a7b3a8
varunriyer <rvaruniyer@gmail.com>
2025-06-19 12:31:21 +0530

    [Varun] feat: Implement register component with login/signup functionality and styles

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatTabsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-
+  selectedTab: 'login' | 'signup' = 'login';
+  isLogin: boolean = true;
+  showPassword = false;
 }
