0814c2250e9b03cf39321492dec0030b46a10862
varunriyer <rvaruniyer@gmail.com>
2025-06-20 12:20:42 +0530

    [Varun] feat: Add profile route to HomeComponent for improved navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,17 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
     component: DashboardComponent,
-    children: [{ path: '', component: HomeComponent }],
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+    ],
   },
   { path: 'login', component: RegisterComponent },
 ];
