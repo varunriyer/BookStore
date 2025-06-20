000c36eb884ab378be66056deb7c63774cdbe04a
varunriyer <rvaruniyer@gmail.com>
2025-06-20 10:30:01 +0530

    [Varun] feat: Refactor HomeComponent to include book grid layout and display book cards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { BookCardComponent } from '../book-card/book-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [BookCardComponent],
+  imports: [BookCardComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  bookList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+}
