bc954a59551b96202d7cffeb34288d3e1fd3fb6b
varunriyer <rvaruniyer@gmail.com>
2025-06-20 09:22:34 +0530

    [Varun] feat: Update dashboard component to include router outlet for navigation

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AppbarComponent } from '../../components/appbar/appbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [AppbarComponent],
+  imports: [AppbarComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
