30309157d3c825ab20614de9118c74a65d5082d3
varunriyer <rvaruniyer@gmail.com>
2025-06-20 11:22:34 +0530

    [Varun] feat: Add tooltip functionality to BookCardComponent

--- src/app/components/book-card/book-card.component.ts
+++ src/app/components/book-card/book-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-book-card',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatTooltipModule],
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.css',
 })
