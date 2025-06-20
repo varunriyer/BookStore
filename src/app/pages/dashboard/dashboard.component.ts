70fc2d8d2c6b040239ec463421c642bbf1d76c95
varunriyer <rvaruniyer@gmail.com>
2025-06-20 12:21:02 +0530

    [Varun] feat: Implement dashboard layout with appbar and footer components

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AppbarComponent } from '../../components/appbar/appbar.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [AppbarComponent, RouterOutlet],
+  imports: [AppbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
