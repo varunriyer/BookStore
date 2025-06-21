4bc9ff9f4d4ec2a4a1a787795af754ab992c9030
varunriyer <rvaruniyer@gmail.com>
2025-06-21 10:03:28 +0530

    [Varun] feat: Add conditional footer display and router event handling in DashboardComponent

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { AppbarComponent } from '../../components/appbar/appbar.component';
-import { RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [AppbarComponent, RouterOutlet, FooterComponent],
+  imports: [AppbarComponent, RouterOutlet, FooterComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  hideFooter: boolean = false;
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.hideFooter = event.url.includes('forgot-password');
+      }
+    });
+  } // Router is used to listen to route changes; ActivatedRoute is used when we want dynamic parameters
+}
