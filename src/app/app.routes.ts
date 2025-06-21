c848ff536953e371f1f9e090e0849065a211f241
varunriyer <rvaruniyer@gmail.com>
2025-06-21 11:49:58 +0530

    [Varun] feat: Add route for Forgot Password component in app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { ForgotpassComponent } from './pages/forgotpass/forgotpass.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,7 +13,11 @@ export const routes: Routes = [
     children: [
       { path: '', component: HomeComponent },
       { path: 'profile', component: ProfileComponent },
+      { path: 'forgot-password', component: ForgotpassComponent },
     ],
   },
-  { path: 'login', component: RegisterComponent },
+  {
+    path: 'login',
+    component: RegisterComponent,
+  },
 ];
